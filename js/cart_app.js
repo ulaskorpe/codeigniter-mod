f933c5b0a333c91a393159aa8ced2448a31d9330
ulaskorpe <ulaskorpe@gmail.com>
2018-01-17 21:21:51 +0000

    vue2

--- js/cart_app.js
+++ js/cart_app.js
@@ -3,11 +3,46 @@ window.addEventListener('load',function () {
         el: '#app2',
         data: {
             isLoading :true,
-            message:"msg2"
+            message:"msg2",
+            cart: [],
+            saved : []
 
 
         }
         ,
+        methods:{
+            removeFromCart(index){
+             // alert(index);
+                if(confirm("aloooo ? ")){
+              const item =  this.cart.splice(index,1);
+              this.saved.push(item[0]);
+                }
+          },
+            removeFromSaved(index){
+                const item =this.saved.splice(index,1);
+                this.cart.push(item[0]);
+            }
+
+        },
+
+
+        computed:{
+            cartTotal(){
+                let total =0;
+                this.cart.forEach((item)=>{
+                    total +=parseFloat(item.price,10);
+                })
+                return total.toFixed(2);
+            },
+            savedTotal(){
+                let total =0;
+                this.saved.forEach((item)=>{
+                    total +=parseFloat(item.price,10);
+            })
+                return total.toFixed(2);
+            }
+        },
+
         created() {
 
             setTimeout(()=>{
@@ -17,6 +52,11 @@ window.addEventListener('load',function () {
 
                 console.log(res)
             this.isLoading = false;
+             this.cart = res.cart ;
+             this.saved = res.saved;
+
+
+                //console.log(res);
              })
 
             },1000)
